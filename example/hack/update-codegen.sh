a211f500e1c26b3822f9d410bcb49afb1d0fc423
Niklas Voss <niklas.voss@gmail.com>
2026-01-17 18:19:26 +0100

    cleanup update-codegen.sh and remove unused code

--- example/hack/update-codegen.sh
+++ example/hack/update-codegen.sh
@@ -20,30 +20,6 @@ CODEGEN_PKG=$(go list -m -f '{{.Dir}}' k8s.io/code-generator)
 # shellcheck disable=SC1091 # we trust kube_codegen.sh
 source "${CODEGEN_PKG}/kube_codegen.sh"
 
-
-function qualify-gvs() {
-  APIS_PKG="$1"
-  GROUPS_WITH_VERSIONS="$2"
-  join_char=""
-  res=""
-
-  for GVs in ${GROUPS_WITH_VERSIONS}; do
-    IFS=: read -r G Vs <<<"${GVs}"
-
-    for V in ${Vs//,/ }; do
-      res="$res$join_char$APIS_PKG/$G/$V"
-      join_char=" "
-    done
-  done
-
-  echo "$res"
-}
-
-
-CLIENT_VERSION_GROUPS="foo:v1alpha1"
-ALL_VERSION_GROUPS="$CLIENT_VERSION_GROUPS"
-
-
 kube::codegen::gen_helpers \
     --boilerplate "${SCRIPT_DIR}/boilerplate.go.txt" \
     "${PROJECT_DIR}/api"
@@ -65,23 +41,6 @@ echo "Setting permissions for files of relevant go modules to 644"
 for MOD in "${GOMODS[@]}"; do
   find "$(go list -json -m -u "${MOD}" | jq -r '.Dir')" -type f -exec chmod 644 -- {} +
 done
-
-
-# TODO: use kube::codegen::gen_openapi, see commented block below (works and generates same code but exit code != 0)
-# mapfile -t input_dirs < <(qualify-gvs "${THIS_PKG}/api" "$ALL_VERSION_GROUPS")
-# "$OPENAPI_GEN" \
-#     --output-dir "$PROJECT_DIR/client-go/openapi" \
-#     --output-pkg "${THIS_PKG}/client-go/openapi" \
-#     --output-file "zz_generated.model_name.go" \
-#     --output-model-name-file "zz_generated.model_name.go" \
-#     --report-filename "$PROJECT_DIR/client-go/openapi/api_violations.report" \
-#     --go-header-file "$SCRIPT_DIR/boilerplate.go.txt" \
-#     "k8s.io/apimachinery/pkg/apis/meta/v1" \
-#     "k8s.io/apimachinery/pkg/runtime" \
-#     "k8s.io/apimachinery/pkg/version" \
-#     "k8s.io/api/core/v1" \
-#     "k8s.io/apimachinery/pkg/api/resource" \
-#     "${input_dirs[@]}"
 
 kube::codegen::gen_openapi \
     --output-dir "${PROJECT_DIR}/client-go/openapi" \
